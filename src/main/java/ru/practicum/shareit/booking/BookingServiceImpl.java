53b32a6d147dc568352b83c39dc4896ada471830
Denis <denisedikh@yandex.ru>
2024-09-23 21:42:14 +0300

    1. changed services to repositories 2. corrected methods of booking repositories in methods "getAllByOwner","getAllByState" 3. added new method "checkUserIsContained"

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -3,16 +3,18 @@ package ru.practicum.shareit.booking;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.NewBookingDto;
+import ru.practicum.shareit.exception.ConditionsNotMetException;
 import ru.practicum.shareit.exception.InvalidRequestException;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.Item;
-import ru.practicum.shareit.item.ItemService;
+import ru.practicum.shareit.item.ItemRepository;
 import ru.practicum.shareit.user.User;
-import ru.practicum.shareit.user.UserService;
+import ru.practicum.shareit.user.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,8 +26,8 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class BookingServiceImpl implements BookingService {
     private final BookingRepository bookingRepository;
-    private final UserService userService;
-    private final ItemService itemService;
+    private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
 
     @Override
     @Transactional
@@ -33,8 +35,11 @@ public class BookingServiceImpl implements BookingService {
         log.debug("Started checking contains booker with bookerId {} and Item in with itemId {} method create",
                 bookerId,
                 newBookingDto.getItemId());
-        final User booker = userService.checkUserById(bookerId);
-        final Item item = itemService.checkItemById(newBookingDto.getItemId());
+        final User booker = checkUserIsContained(bookerId);
+        final Item item = itemRepository.findById(newBookingDto.getItemId()).orElseThrow(() -> {
+            log.warn("Item with id {} not found ", newBookingDto.getItemId());
+            return new NotFoundException(String.format("Item with id = %d not found ", newBookingDto.getItemId()));
+        });
         checkTime(newBookingDto);
         checkAvailable(item);
         log.debug("Finished checking contains booker with bookerId {} and Item in with itemId {} method create",
@@ -49,6 +54,10 @@ public class BookingServiceImpl implements BookingService {
     public BookingDto update(Long bookingId, Long userId, Boolean approved) {
         final Booking booking = bookingRepository.getReferenceById(bookingId);
         final Item item = booking.getItem();
+        if (booking.getStatus() != Status.WAITING) {
+            log.warn("Status must be WAITING");
+            throw new ConditionsNotMetException("Status must be WAITING");
+        }
         if (!item.getOwner().getId().equals(userId)) {
             log.warn("Only owner can change status");
             throw new InvalidRequestException("Only owner can change status");
@@ -61,6 +70,9 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public BookingDto getById(Long bookingId, Long userId) {
+        log.debug("Started checking contains user with userId {} in method getById", userId);
+        checkUserIsContained(userId);
+        log.debug("Finished checking contains user with userId {} in method getById", userId);
         BooleanExpression byId = QBooking.booking.id.eq(bookingId);
         BooleanExpression byOwner = QBooking.booking.item.owner.id.eq(userId);
         BooleanExpression byBooker = QBooking.booking.booker.id.eq(userId);
@@ -74,43 +86,45 @@ public class BookingServiceImpl implements BookingService {
     @Override
     public List<BookingDto> getAllByState(Long bookerId, State state) {
         log.debug("Started checking contains user with bookerId {} in method getAllByState", bookerId);
-        userService.checkUserById(bookerId);
+        checkUserIsContained(bookerId);
         log.debug("Finished checking contains user with bookerId {} in method getAllByState", bookerId);
         LocalDateTime ldt = LocalDateTime.now();
+        Sort sort = Sort.by(Sort.Direction.DESC, "start");
         return switch (state) {
             case ALL -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByBookerIdOrderByStartDesc(bookerId));
+                    .findBookingByBookerId(bookerId, sort));
             case WAITING -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByBookerIdAndStatusOrderByStartDesc(bookerId, Status.WAITING));
+                    .findBookingByBookerIdAndStatus(bookerId, Status.WAITING, sort));
             case REJECTED -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByBookerIdAndStatusOrderByStartDesc(bookerId, Status.REJECTED));
+                    .findBookingByBookerIdAndStatus(bookerId, Status.REJECTED, sort));
             case CURRENT -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByBookerIdAndStartBeforeAndEndAfterOrderByStartDesc(bookerId, ldt, ldt));
+                    .findBookingByBookerIdAndStartBeforeAndEndAfter(bookerId, ldt, ldt, sort));
             case PAST -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByBookerIdAndEndBeforeOrderByStartDesc(bookerId, ldt));
+                    .findBookingByBookerIdAndEndBefore(bookerId, ldt, sort));
             case FUTURE -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByBookerIdAndStartAfterOrderByStartDesc(bookerId, ldt));
+                    .findBookingByBookerIdAndStartAfter(bookerId, ldt, sort));
         };
     }
 
     @Override
     public List<BookingDto> getAllByOwner(Long ownerId, State state) {
         log.debug("Started checking contains user with bookerId {} in method getAllByOwner", ownerId);
-        userService.checkUserById(ownerId);
+        checkUserIsContained(ownerId);
         log.debug("Finished checking contains user with bookerId {} in method getAllByOwner", ownerId);
         LocalDateTime ldt = LocalDateTime.now();
+        Sort sort = Sort.by(Sort.Direction.DESC, "start");
         return switch (state) {
-            case ALL -> BookingMapper.toBookingDto(bookingRepository.findBookingByItemOwnerIdOrderByStartDesc(ownerId));
+            case ALL -> BookingMapper.toBookingDto(bookingRepository.findBookingByItemOwnerId(ownerId, sort));
             case WAITING -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByItemOwnerIdAndStatusOrderByStartDesc(ownerId, Status.WAITING));
+                    .findBookingByItemOwnerIdAndStatus(ownerId, Status.WAITING, sort));
             case REJECTED -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByItemOwnerIdAndStatusOrderByStartDesc(ownerId, Status.REJECTED));
+                    .findBookingByItemOwnerIdAndStatus(ownerId, Status.REJECTED, sort));
             case CURRENT -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByItemOwnerIdAndStartBeforeAndEndAfterOrderByStartDesc(ownerId, ldt, ldt));
+                    .findBookingByItemOwnerIdAndStartBeforeAndEndAfter(ownerId, ldt, ldt, sort));
             case PAST -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByItemOwnerIdAndEndBeforeOrderByStartDesc(ownerId, ldt));
+                    .findBookingByItemOwnerIdAndEndBefore(ownerId, ldt, sort));
             case FUTURE -> BookingMapper.toBookingDto(bookingRepository
-                    .findBookingByItemOwnerIdAndStartAfterOrderByStartDesc(ownerId, ldt));
+                    .findBookingByItemOwnerIdAndStartAfter(ownerId, ldt, sort));
         };
     }
 
@@ -129,5 +143,13 @@ public class BookingServiceImpl implements BookingService {
             log.warn("Booking has invalid time");
             throw new InvalidRequestException("Booking has invalid time");
         }
+    }
+
+    private User checkUserIsContained(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> {
+                    log.warn("User with id {} not found", userId);
+                    return new NotFoundException(String.format("User with id = %d not found", userId));
+                });
     }
 }
