b4f2b447d9984ab0db4394eedd34752f09904c1e
Denis <denisedikh@yandex.ru>
2024-09-23 21:38:29 +0300

    deleted constructor, field and number of enum

--- src/main/java/ru/practicum/shareit/booking/Status.java
+++ src/main/java/ru/practicum/shareit/booking/Status.java
@@ -1,16 +1,7 @@
 package ru.practicum.shareit.booking;
 
-import lombok.Getter;
-
-@Getter
 public enum Status {
-    APPROVED(1),
-    WAITING(2),
-    REJECTED(3);
-
-    private final int value;
-
-    Status(int value) {
-        this.value = value;
-    }
+    APPROVED,
+    WAITING,
+    REJECTED;
 }
