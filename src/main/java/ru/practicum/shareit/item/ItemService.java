ca36461969be7b90d8644ef826ce034eabd94f84
Denis <denisedikh@yandex.ru>
2024-09-23 21:47:02 +0300

    deleted unused method "checkItemById"

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -1,9 +1,10 @@
 package ru.practicum.shareit.item;
 
-import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.comment.Comment;
+import ru.practicum.shareit.comment.dto.CommentDto;
+import ru.practicum.shareit.comment.dto.NewCommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemDtoTime;
-import ru.practicum.shareit.item.dto.NewCommentDto;
 import ru.practicum.shareit.item.dto.NewItemDto;
 import ru.practicum.shareit.item.dto.UpdateItemDto;
 
@@ -17,8 +18,6 @@ public interface ItemService {
     List<ItemDto> getAllByUserId(Long userId);
 
     ItemDtoTime getById(Long itemId);
-
-    Item checkItemById(Long itemId);
 
     List<ItemDto> getBySearch(Long userId, String text);
 
