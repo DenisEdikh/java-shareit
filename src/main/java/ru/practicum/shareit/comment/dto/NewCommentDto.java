b5b031d10e8aec83abbc4d593f48c1e3dc0c73c4
Denis <denisedikh@yandex.ru>
2024-09-23 21:42:59 +0300

    changed package of comment in import

--- src/main/java/ru/practicum/shareit/comment/dto/NewCommentDto.java
+++ src/main/java/ru/practicum/shareit/comment/dto/NewCommentDto.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.item.dto;
+package ru.practicum.shareit.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoTime.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoTime.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.comment.dto.CommentDto;
 
 import java.util.HashSet;
 import java.util.List;
